fd29ca0fc0592a7334953efd53c2f0f9f6294afc
crathana <clement.rathana@gmail.com>
2024-05-06 01:36:39 +0200

    Avancement sur le code + nouvelle table des types en csv qui est complete

--- Code_pokemons/pokemon.py
+++ Code_pokemons/pokemon.py
@@ -1,30 +1,29 @@
 import pandas as pd
 import numpy as np
 import ast
+from abc import ABC, abstractmethod
 
 # On recupere les données dont j'ai besoin qu'on met dans un dataframe
-
-pokemon_csv = pd.read_csv("C:\ENSG\Projet_Info\RATHANA-BOUCHART-KACZOR\data\pokemon_first_gen.csv", skiprows=1)#
-pokemon_position=pd.read_csv("C:\ENSG\Projet_Info\RATHANA-BOUCHART-KACZOR\data\pokemon_coordinates.csv",skiprows=1)
-
+pokemon_stat_df = pd.read_csv("C:\ENSG\Projet_Info\RATHANA-BOUCHART-KACZOR\data\pokemon_first_gen.csv", skiprows=1)
+pokemon_position_df=pd.read_csv("C:\ENSG\Projet_Info\RATHANA-BOUCHART-KACZOR\data\pokemon_coordinates.csv",skiprows=1)
+table_des_types_df=pd.read_csv("C:\ENSG\Projet_Info\RATHANA-BOUCHART-KACZOR\data\table_des_types.csv",skiprows=1)
 
 # Je les convertis en matrices pour mieux les manipuler
-
-pokemon_stat=pokemon_csv.values
-pokemon_pos=pokemon_position.values
-
+pokemon_stat=pokemon_stat_df.values
+pokemon_pos=pokemon_position_df.values
+table_types=table_des_types_df.values
 
 #J'arrondis les valeurs des coordonnées des pokemons après les avoir multipliées par 10 pour éviter les conflits (2 pokemons par case par exemple):
-
 pokemon_pos_arrondies=np.copy(pokemon_pos)
 coord=np.array([ast.literal_eval(pos) for pos in pokemon_pos[:, 1]]) # ast.literal_eval va transformer les chaînes de caractère de listes en listes
 pokemon_pos_arrondies= np.round(coord * 10, decimals=0).astype(int)
 
 
-#Création d'un dictionnaire avec comme clés le nom des pokemons auxquels otn associe leur numéro dans le Pokedex, pour pouvoir optimiser les recherches
-
+#Création d'un dictionnaire avec comme clés les noms des pokemons auxquels on associe leur numéro dans le Pokedex, pour pouvoir optimiser les recherches
 dico_pokemon={row[1]:row[0] for row in pokemon_stat}
 
+#Création d'un dictionnaire avec comme clés les types auxquels on associe leur place dans le tableau des types 
+dico_types={table_types[0][i+1]: i for i in range(len(table_types)-1)}
 
 class Pokemon:
     #Initialisation des pokemons
@@ -39,6 +38,10 @@ class Pokemon:
         self.speed=pokemon_stat[nb_pkm][9]
         self.generation=pokemon_stat[nb_pkm][10]
         self.legendary=pokemon_stat[nb_pkm][11]
+
+    @abstractmethod
+    def attaque(self,pokemon_adverse):
+        pass
         
 
 
