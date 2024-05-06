d4667dfd4af0ae58e140475b07c6f45a0a4888e3
cbouchart <bouchart.cel@gmail.com>
2024-05-06 10:18:42 +0200

    affichage d'un perso + ajout d'un spritesheet

--- Code_dresseur/affichage.py
+++ Code_dresseur/affichage.py
@@ -1,10 +1,24 @@
 from PIL import Image
 from matplotlib import pyplot as plt
 
-# Charger l'image depuis le fichier
-image = Image.open("C:/Users/dell/OneDrive/Bureau/Projet Pokémon/RATHANA-BOUCHART-KACZOR/documents/images/fond_pokemon.jpg")
+# Charger les images
+fond = Image.open("C:/Users/dell/OneDrive/Bureau/Projet Pokémon/RATHANA-BOUCHART-KACZOR/documents/images/fond_pokemon.jpg")
+planche = Image.open("C:/Users/dell/OneDrive/Bureau/Projet Pokémon/RATHANA-BOUCHART-KACZOR/documents/images/SpriteSheet.png")
+
+# Découper le Sprite Sheet
+liste_personnages = [(1,0,31,36)]
+
+for personnage in liste_personnages:
+    perso_sprite = planche.crop(personnage).convert("RGBA")
+    perso_redimension = perso_sprite.resize((300, 360))
+    # Superposer le sprite sur le fond
+    fond.paste(perso_redimension, (15, 20))  # Remplacez x_position et y_position par les coordonnées de position sur votre fond
+
+
+
 
 # Afficher l'image
-plt.imshow(image)
+plt.clf()
+plt.imshow(fond, interpolation="none")
 plt.axis('off')  # Masquer les axes
 plt.show()
