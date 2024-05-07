169283ce069bcb6dce17f24ccd1a1f604832279d
crathana <clement.rathana@gmail.com>
2024-05-07 17:13:52 +0200

    code de l'attaque finalisé

--- Code_pokemons/pokemon.py
+++ Code_pokemons/pokemon.py
@@ -3,22 +3,26 @@ import numpy as np
 import ast
 from abc import ABC, abstractmethod
 import os 
+import random as rd
 
 
 path=os.path.dirname(os.path.abspath(__file__))
 path1=os.path.join(path,"..\data\pokemon_first_gen.csv")
 path2=os.path.join(path,"..\data\pokemon_coordinates.csv")
 path3=os.path.join(path,"..\data\Table_des_types.csv")
-
+path4=os.path.join(path,"..\data\Attaques.csv")
 # On recupere les données dont j'ai besoin qu'on met dans un dataframe
-pokemon_stat_df = pd.read_csv(path1, skiprows=1)
-pokemon_position_df=pd.read_csv(path2,skiprows=1)
+pokemon_stat_df = pd.read_csv(path1)
+pokemon_stat_df=pokemon_stat_df.fillna('Rien') #Remplacer les valeurs NaN par 'Rien'
+pokemon_position_df=pd.read_csv(path2)
 table_des_types_df=pd.read_csv(path3)
+nom_attaques_df=pd.read_csv(path4)
 
 # Je les convertis en matrices pour mieux les manipuler
 pokemon_stat=pokemon_stat_df.values
 pokemon_pos=pokemon_position_df.values
 table_types=table_des_types_df.values
+nom_attaques=nom_attaques_df.values
 
 #J'arrondis les valeurs des coordonnées des pokemons après les avoir multipliées par 10 pour éviter les conflits (2 pokemons par case par exemple):
 pokemon_pos_arrondies=np.copy(pokemon_pos)
@@ -38,8 +42,9 @@ table_types= np.delete(table_types, 0, axis=1)
 class Pokemon:
     #Initialisation des pokemons
     def __init__(self,nom):
-        nb_pkm=dico_pokemon[nom]
-        self.nom=nom
+        nb_pkm=dico_pokemon[nom] -1
+        self.npokedex=nb_pkm
+        self.name=pokemon_stat[nb_pkm][1]
         self.type1= pokemon_stat[nb_pkm][2]
         self.type2=pokemon_stat[nb_pkm][3]
         self.HP=pokemon_stat[nb_pkm][5]
@@ -52,6 +57,16 @@ class Pokemon:
         self.legendary=pokemon_stat[nb_pkm][12]
 
     def attaque(self,pokemon_adverse):
+        if self.attack>=self.sp_atk:
+            nom_attaque_normale=nom_attaques[0][0]
+            nom_attaque_type=nom_attaques[dico_types[self.type1]][0]
+            stat_attaque=self.attack
+            stat_defense=self.defense
+        else:
+            nom_attaque_normale=nom_attaques[0][1]
+            nom_attaque_type=nom_attaques[dico_types[self.type1]][1]
+            stat_attaque=self.sp_atk
+            stat_defense=self.spe_def
         attaque=int(input("Quelle attaque choisir: L'attaque 1 (type normal) ou l'attaque 2 (type de votre pokemon)? :"))
         while attaque != 1 and attaque !=2:
             attaque=int(input("Choississez soit l'attaque 1 soit l'attaque 2 (écrivez 1 ou 2):"))
@@ -59,17 +74,21 @@ class Pokemon:
             type_attaque=dico_types["Normal"]
         else:
             type_attaque=dico_types[self.type1]
-        type_defense1=dico_types[pokemon_adverse.type1]  #On recupere le type de defense
-        type_defense2=dico_types[pokemon_adverse.type2]
+            if self.type1=="Normal" and not self.type2 != "Rien": # Definir le type de l'attaque comme le type 2, s'il existe et que le type 1 est "Normal"
+                type_attaque=self.type2
+
+        type_defense1=dico_types[pokemon_adverse.type1] #On recupere le type de defense
+        if pokemon_adverse.type2 != "Rien":
+            type_defense2=dico_types[pokemon_adverse.type2]
         #On applique la formule officielle des degats des attaques, mais sans prendre en compte les niveaux
         CM=table_types[type_attaque][type_defense1]
-        if not np.isnan(type_defense2):
-            CM*=type_defense2
-        degats= np.trunc((np.trunc((self.attack * 40)/(pokemon_adverse.defense *50))+2) * CM)
+        if pokemon_adverse.type2 != "Rien":
+            CM*=table_types[type_attaque][type_defense2]
+        degats= np.trunc((np.trunc((5*stat_attaque * 55)/(stat_defense *50))+2) * CM)
         pokemon_adverse.HP=pokemon_adverse.HP - degats
         if pokemon_adverse.HP<0:
             pokemon_adverse.HP=0
-        return pokemon_adverse.HP, degats
+        return pokemon_adverse.HP, degats, nom_attaque_normale, nom_attaque_type
         
 
 
@@ -82,16 +101,33 @@ class PokemonSauvage(Pokemon):
         self.y=y 
 
     def attaque(self,pokemon_adverse):
-        return super().attaque(pokemon_adverse)
+        type_attaque=""
+        nb_aleatoire=rd.randint(1,2)
+        if nb_aleatoire==1:
+            type_attaque=dico_types["Normal"]
+        if nb_aleatoire==2:
+            type_attaque=dico_types[self.type1]
+            if self.type1=="Normal" and self.type2 !="Rien":
+                type_attaque=dico_types[self.type2]
+        type_defense1=dico_types[pokemon_adverse.type1] #On recupere le type de defense
+        if pokemon_adverse.type2 != "Rien":
+            type_defense2=dico_types[pokemon_adverse.type2]
+        #On applique la formule officielle des degats des attaques, mais sans prendre en compte les niveaux
+        CM=table_types[type_attaque][type_defense1]
+        if pokemon_adverse.type2 != "Rien":
+            CM*=table_types[type_attaque][type_defense2]
+        degats= np.trunc((np.trunc((2*stat_attaque * 55)/(stat_defense *50))+2) * CM)
+        pokemon_adverse.HP=pokemon_adverse.HP - degats
+        if pokemon_adverse.HP<0:
+            pokemon_adverse.HP=0
+        return pokemon_adverse.HP, degats, type_attaque
+
 
 if __name__=="__main__":
-    nidoking=Pokemon("Venusaur")
-    print(nidoking.type2)
-    pikachu=Pokemon("Pikachu")
-    print(pikachu.type2)
-
-
-
+    mew=Pokemon("Mew")
+    florizare=Pokemon("Venusaur")
+    print(mew.attaque(florizare))
+    print(florizare.attaque(mew))
 
 
 
