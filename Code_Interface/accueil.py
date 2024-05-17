eae215f5bc03194bd6d23e9b3eb4e59c20c3bdf8
elkac <kaczorvirgile3@gmail.com>
2024-05-17 18:01:43 +0200

    test

--- Code_Interface/accueil.py
+++ Code_Interface/accueil.py
@@ -1,8 +1,11 @@
 import sys
+import os
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 from PyQt5.QtCore import QUrl, Qt, pyqtSignal
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtGui import QPixmap
+
+path = os.path.dirname(os.path.abspath(__file__))
 
 class Fenetre(QMainWindow):
 
@@ -20,18 +23,18 @@ class Fenetre(QMainWindow):
         self.music_player = Musique()  # Ajout de l'objet Music
 
     def afficherFond(self):
-        pixmap = QPixmap("C:/Users/dell/OneDrive/Bureau/Projet Pokémon/RATHANA-BOUCHART-KACZOR/documents/images/accueil.png")
+        pixmap = QPixmap(os.path.join(path, "../documents/images/accueil.png"))
         self.fond = QLabel(self)
         self.fond.setPixmap(pixmap)
         self.fond.setGeometry(0, 0, pixmap.width(), pixmap.height())
 
-        pixmap = QPixmap("C:/Users/dell/OneDrive/Bureau/Projet Pokémon/RATHANA-BOUCHART-KACZOR/documents/images/dialogue.png")
+        pixmap = QPixmap(os.path.join(path, "../documents/images/dialogue.png"))
         self.dialogue = QLabel(self)
         self.dialogue.setPixmap(pixmap)
         self.dialogue.setGeometry(100, 405, pixmap.width(), pixmap.height())
 
     def afficherSorbier(self):
-        pixmap = QPixmap("C:/Users/dell/OneDrive/Bureau/Projet Pokémon/RATHANA-BOUCHART-KACZOR/documents/images/sorbier.png")
+        pixmap = QPixmap(os.path.join(path, "../documents/images/sorbier.png"))
         self.sorbier = QLabel(self)
         self.sorbier.setPixmap(pixmap)
         self.sorbier.setGeometry(100, 200, pixmap.width(), pixmap.height())
@@ -80,7 +83,7 @@ class Musique():
         self.chargerEtJouerMusique()
 
     def chargerEtJouerMusique(self):
-        chemin_intro = "C:/Users/dell/OneDrive/Bureau/Projet Pokémon/RATHANA-BOUCHART-KACZOR/son/intro.mp3"
+        chemin_intro = os.path.join(path,"../son/intro.mp3")
         self.intro.setMedia(QMediaContent(QUrl.fromLocalFile(chemin_intro)))
         self.intro.setVolume(50)
         self.intro.play()
@@ -89,14 +92,14 @@ class Pokeball(QLabel):
     def __init__(self, parent=None, poke_type=""):
         super().__init__(parent)
         self.poke_type = poke_type
-        self.setPixmap(QPixmap(f"C:/Users/dell/OneDrive/Bureau/Projet Pokémon/RATHANA-BOUCHART-KACZOR/documents/images/pokeballFermee.png"))
+        self.setPixmap(QPixmap(os.path.join(path, f"../documents/images/pokeballFermee.png")))
         self.setMinimumHeight(129)
 
     def enterEvent(self, event):
-        self.setPixmap(QPixmap(f"C:/Users/dell/OneDrive/Bureau/Projet Pokémon/RATHANA-BOUCHART-KACZOR/documents/images/{self.poke_type}Ouverte.png"))
+        self.setPixmap(QPixmap(os.path.join(path, f"../documents/images/{self.poke_type}Ouverte.png")))
 
     def leaveEvent(self, event):
-        self.setPixmap(QPixmap(f"C:/Users/dell/OneDrive/Bureau/Projet Pokémon/RATHANA-BOUCHART-KACZOR/documents/images/pokeballFermee.png"))
+        self.setPixmap(QPixmap(os.path.join(path, f"../documents/images/pokeballFermee.png")))
 
 class ClickableLabel(QLabel):
     clicked = pyqtSignal()
