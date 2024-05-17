d7ec03f36d1f938b9b57031fdb58cc3cde9036aa
crathana <clement.rathana@gmail.com>
2024-05-17 20:48:12 +0200

    Tout marche pour les combats normalement dernier push

--- Interface.py
+++ Interface.py
@@ -110,9 +110,9 @@ class Window(QMainWindow):
         dracolosse=pk.Pokemon("Dragonite")
         papilusion=pk.Pokemon("Butterfree")
         arcanin=pk.Pokemon("Arcanine")
-        mewtwo=pk.Pokemon("Ditto")
+        mewtwo=pk.Pokemon("Snorlax")
         vide=pk.Pokemon("Vide")
-        self.equipe_dresseur = [salameche, mew, papilusion, arcanin,dracolosse,vide]
+        self.equipe_dresseur = [mewtwo, vide, vide, vide,vide,vide]
         self.inventaire = []
         
                 
